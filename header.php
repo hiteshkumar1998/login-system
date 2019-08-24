d7986d2142d22569135ce832ebe59811ee92a240
hiteshkumar1998 <54476509+hiteshkumar1998@users.noreply.github.com>
2019-08-24 22:35:22 +0530

    navbar-brand change
    navbar-brand change

--- header.php
+++ header.php
@@ -15,7 +15,7 @@ session_start();
 	<nav class="navbar navbar-inverse">
 	  <div class="container-fluid">
 	    <div class="navbar-header">
-	      <a class="navbar-brand" href="#">Website</a>
+	      <a class="navbar-brand" href="#">Website Name</a>
 	    </div>
 	    <ul class="nav navbar-nav">
 	      <li class="active"><a href="#">Home</a></li>
